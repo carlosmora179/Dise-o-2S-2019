459d42eb2d6348329a4864382dc47946dd121645
carlos <carlosmora179@gmail.com>
2019-11-10 19:20:31 -0600

    Proyecto base developer
    proyecto base

---
+++ disenioproyecto/src/main/java/com/disenio/disenioproyecto/DisenioproyectoApplication.java
@@ -0,0 +1,13 @@
+package com.disenio.disenioproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisenioproyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisenioproyectoApplication.class, args);
+    }
+
+}
